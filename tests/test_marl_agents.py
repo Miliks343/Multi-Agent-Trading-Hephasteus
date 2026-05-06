6b5b63388bb17ee66b29169bf6b26230c80a13fd
Pavel Jordanov <0x706a@gmail.com>
2026-05-06 19:03:08 +0000

    rename marl_coordinator to marl_agents

--- tests/test_marl_agents.py
+++ tests/test_marl_agents.py
@@ -11,7 +11,7 @@ from unittest.mock import MagicMock
 import numpy as np
 import pytest
 
-from marl_lob.marl_coordinator import (
+from marl_lob.marl_agents import (
     ChildSnapshot,
     ExecutedFill,
     build_per_agent_state,
